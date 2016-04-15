678d9f835d7d844f8aa97b248cbbc86abea91c52
alessandro308 <alessandropagiaro@gmail.com>
2016-04-15 12:26:55 +0200

    From cURL to Java

--- src/s2t/Bot.java
+++ src/s2t/Bot.java
@@ -3,15 +3,18 @@ package s2t;
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.omg.CORBA.NameValuePair;
+import sun.jvm.hotspot.debugger.cdbg.basic.BasicNamedFieldIdentifier;
 
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.net.*;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 
@@ -22,7 +25,7 @@ public class Bot {
 
     public Bot() throws IOException {
         //Prendiamo l'ultimo update_ID
-        int lastOffset = 0; getUpdateID().lastElement();
+        int lastOffset = 0;  getUpdateID().lastElement();
 
         while(true){
 
@@ -36,9 +39,17 @@ public class Bot {
                     JSONObject filePath = (JSONObject) callJSON(new URL(url+"getFile?file_id="+file.get("file_id"))).get("result");
                     URL fileToGet = new URL("https://api.telegram.org/file/bot"+TOKEN+"/"+filePath.get("file_path"));
                     //Download File
+                    //System.out.println(fileToGet);
                     ReadableByteChannel rbc = Channels.newChannel(fileToGet.openStream());
-                    FileOutputStream fos = new FileOutputStream((String) file.get("file_id"));
+                    FileOutputStream fos = new FileOutputStream(file.get("file_id") + ".oga");
                     fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                    Process p = Runtime.getRuntime().exec("opusdec --rate 16000 "+file.get("file_id")+".oga"+" "+file.get("file_id")+".wav");
+                    try {
+                        p.waitFor();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(googleTranscription(file.get("file_id")+".wav"));
                 }
             }
             try {
@@ -100,6 +111,39 @@ public class Bot {
         } catch (ParseException e) {
             return null;
         }
+    }
+
+    static JSONObject googleTranscription(String fileName) throws IOException {
+        String chromeDevKey = "AIzaSyCx0Wgvm954KQnHRG7SYJxysjSW59oQ1yk";
+        String url = "https://www.google.com/speech-api/v2/";
+        String params = "recognize?output=json&lang=it-IT&key="+chromeDevKey;
+
+        File binfile = new File(fileName);
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "audio/l16");
+            conn.setRequestProperty("rate", "16000");
+
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            DataOutputStream paramConn = new DataOutputStream(conn.getOutputStream());
+            paramConn.writeBytes(params);
+            paramConn.flush();
+            paramConn.close();
+
+            OutputStream out = conn.getOutputStream();
+            Files.copy(binfile.toPath(), out);
+            out.flush();
+            System.out.println(conn.getResponseMessage());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return null;
     }
 
     public static void main(String[] args){
