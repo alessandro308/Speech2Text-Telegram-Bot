700fb3942b7663f33d835ee5fe9bee75b0225338
alessandro308 <alessandropagiaro@gmail.com>
2016-04-16 13:25:26 +0200

    Param example file

---
+++ src/s2t/PARAM_example.java
@@ -0,0 +1,9 @@
+package s2t;
+
+/**
+ * Created by alessandro on 16/04/16.
+ */
+public class PARAM {
+    static final String chromeKey1 = "******<your-chromium-dev-key>";
+    static final String botToken = "19************<your-bot-token>";
+}
